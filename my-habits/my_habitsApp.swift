672a5fd079b12674da76bedd12f28f62ee9aa989
ThalesMonteir0 <thales.monteiro.moreira@gmail.com>
2025-10-19 01:10:45 -0300

    Initial Commit

---
+++ my-habits/my_habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  my_habitsApp.swift
+//  my-habits
+//
+//  Created by Thales Monteiro on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct my_habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
